84cec2472ba5482a945dcf841a9a7fc6eb8eaa7f
Đào Duy Tùng <duytung.developer@gmail.com>
2025-03-05 13:46:52 +0700

    update menu

--- Web/src/components/Layouts/sidebar/data/index.ts
+++ Web/src/components/Layouts/sidebar/data/index.ts
@@ -5,7 +5,7 @@ export const NAV_DATA = [
     label: "MAIN MENU",
     items: [
       {
-        title: "Dashboard",
+        title: "Báo cáo và Phân tích Bán hàng",
         icon: Icons.HomeIcon,
         items: [
           {
@@ -14,6 +14,79 @@ export const NAV_DATA = [
           },
         ],
       },
+      {
+        title: "Sản phẩm",
+        icon: Icons.Table,
+        items: [
+          {
+            title: "Quản lý Danh mục Sản phẩm",
+            url: "/products/manager",
+          },
+          {
+            title: "Tìm kiếm Sản phẩm Nâng cao",
+            url: "/products/advanceFind",
+          },
+          {
+            title: "Quản lý Tồn kho",
+            url: "/products/manager2",
+          },
+        ],
+      },
+
+      {
+        title: "Người dùng",
+        icon: Icons.Table,
+        items: [
+          {
+            title: "Quản lý Người dùng",
+            url: "/user/manager",
+          },
+          {
+            title: "Đăng ký người dùng",
+            url: "/user/register",
+          },
+        ],
+      },
+
+      {
+        title: "Đặt hàng",
+        icon: Icons.Table,
+        items: [
+          {
+            title: "Đặt hàng",
+            url: "/order/placeOrder",
+          },
+          {
+            title: "Mô phỏng Đặt Hàng Cạnh tranh",
+            url: "/order/advanceOrder",
+          },
+        ],
+      },
+
+      {
+        title: "Lịch sử Đơn hàng và Chi tiết",
+        url: "/order/manager",
+        icon: Icons.Table,
+        items: [
+        ],
+      },
+
+      {
+        title: "Dữ liệu",
+        icon: Icons.Table,
+        items: [
+          {
+            title: "Sao lưu Dữ liệu",
+            url: "/data/backup",
+          },
+          {
+            title: "Phục hồi Dữ liệu",
+            url: "/data/restore",
+          },
+        ],
+      },
+
+
       {
         title: "Calendar",
         url: "/calendar",
