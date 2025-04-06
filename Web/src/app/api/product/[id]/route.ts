ca67f8aef4513b2afbd234885f0bebc21633895e
Đào Duy Tùng <duytung.developer@gmail.com>
2025-04-06 17:21:41 +0700

    Fix code build

--- Web/src/app/api/product/[id]/route.ts
+++ Web/src/app/api/product/[id]/route.ts
@@ -3,10 +3,11 @@ import { Product } from "@/models";
 import { NextRequest } from "next/server";
 import { ObjectId } from "mongodb";
 
-await connectToDatabase();
+connectToDatabase();
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    const product = await Product.findById(new ObjectId(params.id));
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const productId = (await params).id;
+    const product = await Product.findById(new ObjectId(productId));
     return Response.json({
         stock: product?.stock ?? 0,
     });
