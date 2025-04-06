5fbc2ef9951c98d6f0d68124f2d102b366ef587f
Đào Duy Tùng <duytung.developer@gmail.com>
2025-04-07 06:50:19 +0700

    limit to 5 , to make request faster

--- Web/src/app/api/customers/route.ts
+++ Web/src/app/api/customers/route.ts
@@ -5,6 +5,6 @@ import { NextRequest } from "next/server";
 await connectToDatabase();
 
 export async function GET(request: NextRequest) {
-    const list = await User.find().limit(10);
+    const list = await User.find().limit(5);
     return Response.json({ data: list });
 }
